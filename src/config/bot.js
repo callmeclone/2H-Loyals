e3afc32d5df2a1a2789345ea9291a6af4934b98c
callmeclone <ticoparker357@gmail.com>
2026-04-26 16:10:55 -0500

    new

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,10 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Watching Madi2hottyy",
+      
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
